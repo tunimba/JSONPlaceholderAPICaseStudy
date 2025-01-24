0317bfd90133319350ac751286d12c3b37fa8194
tunahan <aktaytunahan@gmail.com>
2025-01-24 05:14:30 +0300

    Initial Commit

---
+++ JSONPlaceholderAPICaseStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JSONPlaceholderAPICaseStudy
+//
+//  Created by Tunahan Aktay on 24.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JSONPlaceholderAPICaseStudy/JSONPlaceholderAPICaseStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONPlaceholderAPICaseStudyApp.swift
+//  JSONPlaceholderAPICaseStudy
+//
+//  Created by Tunahan Aktay on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct JSONPlaceholderAPICaseStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
