25c2b07e3db90cab409fa8e8631ca6871d149de1
tunahan <aktaytunahan@gmail.com>
2025-01-24 07:02:12 +0300

    Created sub User items

--- JSONPlaceholderAPICaseStudy/Models/UserModel.swift
+++ JSONPlaceholderAPICaseStudy/Models/UserModel.swift
@@ -14,3 +14,31 @@ struct User: Identifiable, Codable {
   let phone: String
   let website: String
 }
+
+struct UserListItem: Identifiable {
+  let id: Int
+  let name: String
+  let email: String
+
+  init(user: User) {
+    self.id = user.id
+    self.name = user.name
+    self.email = user.email
+  }
+}
+
+struct UserDetailItem: Identifiable {
+  let id: Int
+  let name: String
+  let email: String
+  let phone: String
+  let website: String
+
+  init(user: User) {
+    self.id = user.id
+    self.name = user.name
+    self.email = user.email
+    self.phone = user.phone
+    self.website = user.website
+  }
+}
