33bd976ad5bead45f44b0efb03a3a49e70d92935
tunahan <aktaytunahan@gmail.com>
2025-01-24 06:49:10 +0300

    Created LocalDataStorage

--- JSONPlaceholderAPICaseStudy/Models/LocalDataStorage.swift
+++ JSONPlaceholderAPICaseStudy/Models/LocalDataStorage.swift
@@ -12,3 +12,27 @@ protocol UserStorageProtocol {
   func getSingleUser(userId: Int) -> User?
   func saveUsers(users: [User])
 }
+
+class LocalDataStorage: UserStorageProtocol {
+  private let defaults = UserDefaults.standard
+  private let key = "saved_users"
+
+  func getAllUsers() -> [User] {
+    guard let data = defaults.data(forKey: key),
+          let users = try? JSONDecoder().decode([User].self, from: data)
+    else {
+      return []
+    }
+    return users
+  }
+
+  func getSingleUser(userId: Int) -> User? {
+    return getAllUsers().first { $0.id == userId }
+  }
+
+  func saveUsers(users: [User]) {
+    if let encoded = try? JSONEncoder().encode(users) {
+      defaults.set(encoded, forKey: key)
+    }
+  }
+}
